bc3615636042113c03dd8aec7c4716905ac7b770
“sukyung6999” <sukyung6999@gmail.com>
2023-11-11 22:42:32 +0900

    add: [처음만난리액트] 11장 Forms

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,12 +12,13 @@ import Accomodate from './chapter_07/Accomodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <div>
-    <AttendanceBook/>
+    <SignUp/>
   </div>
 )
 
