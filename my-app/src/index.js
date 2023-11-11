f12f1f5bfe10167caf356cd36bf04b5e95544109
“sukyung6999” <sukyung6999@gmail.com>
2023-11-11 20:32:53 +0900

    add: [처음만난리액트] 10장 List and Keys

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,12 +11,13 @@ import NotificationList from './chapter_06/NotificationList'
 import Accomodate from './chapter_07/Accomodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <div>
-    <LandingPage/>
+    <AttendanceBook/>
   </div>
 )
 
