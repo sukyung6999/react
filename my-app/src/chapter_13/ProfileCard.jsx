4912040962d5ce5ab2b6d83fc959a1032cf23e72
“sukyung6999” <sukyung6999@gmail.com>
2023-11-12 23:20:11 +0900

    add: [처음만난리액트] 13장 Composition vs Inheritance

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="sukyung lee" backgroundColor="#4ea04c">
+      <p>안녕하세요, 수경입니다</p>
+      <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+    </Card>
+  )
+}
+export default ProfileCard;
