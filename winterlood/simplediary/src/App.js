576ac98dd97cf99cd1a4dba81a79f4c72f00e797
“sukyung6999” <sukyung6999@gmail.com>
2023-11-21 23:06:32 +0900

    add: [한입크기리액트] 섹션 5. React 기본 - 간단한 일기장 프로젝트
    - useMemo

--- winterlood/simplediary/src/App.js
+++ winterlood/simplediary/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useMemo } from 'react';
 import './App.css'
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -46,6 +46,18 @@ function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return {goodCount, badCount, goodRatio};
+  }, [data.length])
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
 
   const getData = async() => {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json())
@@ -96,6 +108,10 @@ function App() {
     <div className="App">
       <Lifecycle/>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
   );
