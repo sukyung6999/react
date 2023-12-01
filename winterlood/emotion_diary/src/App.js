1c7ec8f90e6cd7e6c4d68e84b570f1187f743fe5
“sukyung6999” <sukyung6999@gmail.com>
2023-12-01 15:17:12 +0900

    add: [한입크기리액트] 섹션 6. React 실전 프로젝트 - 감정  일기장
    - 기초공사2

--- winterlood/emotion_diary/src/App.js
+++ winterlood/emotion_diary/src/App.js
@@ -1,27 +1,86 @@
 import './App.css';
+import React , {useReducer, useRef} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
-import RouteTest from './components/RouteTest';
 
 import Home from './pages/Home';
 import New from './pages/New'; 
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT':
+      return action.data;
+    case 'CREATE':
+      newState = [action.data, ...state];
+      break;
+    case 'REMOVE':
+      newState = state.filter((item) => item.id !== action.targetId);
+      break;
+    case 'EDIT':
+      newState = state.map((item) => item.id === action.data.id ? {...action.data} : item);
+      break;
+    default:
+      return state;
+  }
+  return newState
+}
+
+export const DiaryStateContext = React.createContext();
+
+export const DiaryDispatchesContext = React.createContext();
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: 'CREATE', 
+      data: {
+        id: dataId.current++,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+    }})
+  }
+
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type: 'REMOVE',targetId})
+  }
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+  }
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h2>App.js</h2>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/new' element={<New/>}/>
-          <Route path='/edit' element={<Edit/>}/>
-          <Route path='/diary/:id' element={<Diary/>}/>
-        </Routes>
-        <RouteTest/>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchesContext.Provider value={{onCreate, onRemove, onEdit}}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/new' element={<New/>}/>
+              <Route path='/edit' element={<Edit/>}/>
+              <Route path='/diary/:id' element={<Diary/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchesContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
---
+++ winterlood/emotion_diary/src/components/MyButton.js
@@ -0,0 +1,14 @@
+function MyButton({onClick, text, type}) {
+  const btnType = ['positive','negative'].includes(type) ? type : 'default';
+  return (
+    <button 
+    className={["MyButton", `MyButton_${btnType}`].join(" ")} 
+    onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+MyButton.defaultProps = {
+  type: 'default'
+}
+export default MyButton;
