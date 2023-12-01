f0b92ae1dcc4db222c38e68064f6c9ef4ecca72e
“sukyung6999” <sukyung6999@gmail.com>
2023-12-01 17:32:30 +0900

    practice: [한입크기리액트] 섹션 6. React 실전 프로젝트 - 감정  일기장
    - 기초공사2

--- winterlood/practice-emotiondiary/src/App.js
+++ winterlood/practice-emotiondiary/src/App.js
@@ -7,31 +7,82 @@ import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import MyButton from './components/MyButton';
 import MyHeader from './components/MyHeader';
+import React, { useContext, useReducer, useRef } from 'react';
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT':
+      return action.data;
+    case 'CREATE':
+      newState = [action.data, ...state];
+      break;
+    case 'REMOVE': 
+      newState = state.filter((item) => item.id !== action.targetId);
+      break;
+    case 'EDIT':
+      newState = state.map((item) => item.id === action.targetId ? {...action.data} : item);
+      break;
+    default:
+      return state;
+  }
+  return newState;
+}
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchesContext = React.createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  const onCreate = (content, emotion, date) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+      id: dataId.current++,
+      date: new Date(date).getTime(),
+      content,
+      emotion
+    }
+  })
+  }
+
+  const onRemove = (targetId) =>{
+    dispatch({
+      type: 'REMOVE',
+      targetId
+    });
+  }
+
+  const onEdit = (targetId, content, emotion, date) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+  }
+  
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader leftButton={{text: '이전'}} headText={'일기장'} rightButton={{text: '다음', type: 'positive'}} />
-        <h1>App.js</h1>
-        <MyButton text={'기본'} type={'default'} onClick={() => alert('기본 버튼 클릭')}/>
-        <MyButton text={'positive'} type={'positive'} onClick={() => alert('positive 버튼 클릭')}/>
-        <MyButton text={'negative'} type={'negative'} onClick={() => alert('negative 버튼 클릭')}/>
-        <MyButton text={'negative'} type={'dd'} onClick={() => alert('negative 버튼 클릭')}/>
-        <MyButton text={'negative'} onClick={() => alert('negative 버튼 클릭')}/>
-        <img src={process.env.PUBLIC_URL + `./assets/emotion1.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `./assets/emotion2.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `./assets/emotion3.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `./assets/emotion4.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `./assets/emotion5.png`} alt="" />
-        <Routes>
-          <Route path={'/'} element={<Home/>} />
-          <Route path={'/new'} element={<New/>} />
-          <Route path={'/edit'} element={<Edit/>} />
-          <Route path={'/diary/?id&move'} element={<Diary/>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchesContext.Provider value={{onCreate, onRemove, onEdit}}>
+        <BrowserRouter>
+          <div className="App">
+            <h1>App.js</h1>
+            <Routes>
+              <Route path={'/'} element={<Home/>} />
+              <Route path={'/new'} element={<New/>} />
+              <Route path={'/edit'} element={<Edit/>} />
+              <Route path={'/diary/?id&move'} element={<Diary/>} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchesContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
