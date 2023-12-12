80ad32b03c56b7e515cf820de109577b97cb3574
“sukyung6999” <sukyung6999@gmail.com>
2023-12-12 16:26:48 +0900

    add: [UDEMY-React 완벽 가이드] 섹션 4 컴포넌트 기본

---
+++ udemy/01-starting-setup/src/Components/Concept/ConceptItem.js
@@ -0,0 +1,14 @@
+function ConceptItem({
+  title,
+  image,
+  description
+}) {
+  return (
+    <li className="concept">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  )
+}
+export default ConceptItem;
