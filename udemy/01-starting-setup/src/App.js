c7a5c925fc7b6a2df3d023c15b769be36ba324c5
“sukyung6999” <sukyung6999@gmail.com>
2023-12-15 20:53:17 +0900

    add: [UDEMY-React 완벽 가이드] 섹션 9 첫번째 연습 프로젝트
    - 개발 환경 셋팅

--- udemy/01-starting-setup/src/App.js
+++ udemy/01-starting-setup/src/App.js
@@ -1,39 +1,96 @@
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
-import ConceptItem from './Components/Concept/ConceptItem';
-import Header from './Components/Header/Header';
-
-const concepts = [
-  {
-    title: 'Components',
-    image: componentsImage,
-    description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
-  },
-  {
-    title: 'State',
-    image: stateImage,
-    description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
-  },
-  {
-    title: 'Events',
-    image: eventsImage,
-    description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
-  },
-];
+import logo from './assets/investment-calculator-logo.png';
 
 function App() {
+  const calculateHandler = (userInput) => {
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+
+    // do something with yearlyData ...
+  };
+
   return (
     <div>
-      <Header/>
-      <ul id="concepts">
-        {
-          concepts.map((concept, idx) => <ConceptItem key={idx} {...concept}/>)
-        }
-      </ul>
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+
+      <form className="form">
+        <div className="input-group">
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input type="number" id="current-savings" />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input type="number" id="yearly-contribution" />
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input type="number" id="expected-return" />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input type="number" id="duration" />
+          </p>
+        </div>
+        <p className="actions">
+          <button type="reset" className="buttonAlt">
+            Reset
+          </button>
+          <button type="submit" className="button">
+            Calculate
+          </button>
+        </p>
+      </form>
+
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+
+      <table className="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>YEAR NUMBER</td>
+            <td>TOTAL SAVINGS END OF YEAR</td>
+            <td>INTEREST GAINED IN YEAR</td>
+            <td>TOTAL INTEREST GAINED</td>
+            <td>TOTAL INVESTED CAPITAL</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
