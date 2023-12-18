bf377f3a1003452651c38545362910a4221e5ef7
“sukyung6999” <sukyung6999@gmail.com>
2023-12-18 21:54:53 +0900

    add: [UDEMY-React 완벽 가이드] 섹션 10 프래그먼트 작업, Portals & refs

--- udemy/01-starting-setup/src/Components/Error/ErrorModal.js
+++ udemy/01-starting-setup/src/Components/Error/ErrorModal.js
@@ -1,23 +1,47 @@
+import ReactDOM from "react-dom";
+
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = ({ errorHandler }) => {
+  return <div className={classes.backdrop} onClick={errorHandler}></div>;
+};
+
+const ModalOverlay = ({ title, text, errorHandler }) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{text}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={errorHandler}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 function ErrorModal({ title, text, errorHandler }) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={errorHandler}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{text}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={errorHandler}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop errorHandler={errorHandler}></Backdrop>,
+        document.getElementById("backdrop-root")
+      )}
+      ;
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          text={text}
+          errorHandler={errorHandler}
+        ></ModalOverlay>,
+        document.getElementById("modal-root")
+      )}
+      ;
+    </>
   );
 }
 export default ErrorModal;
